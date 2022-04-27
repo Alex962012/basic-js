352b4446a54e7f1012e11c94d343d772772b34e4
alex96 <alex.96_2012@mail.ru>
2022-04-28 04:25:24 +0900

    add solution hanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,9 +14,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi( disksNumber, turnsSpeed ) {
+  
+ disksNumber=(2**disksNumber)-1;
+  let sec=Math.floor(disksNumber/((turnsSpeed/60)/60));
+ let res={
+   turns: disksNumber,
+   seconds: sec
+ }
+return res
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(season) {
+     
+//   if (season instanceof Date){
+//     if(season.getFullYear()<1970){
+//         let b=season.getFullYear()
+
+//         season.setFullYear(b+1000)
+//     }
+    
+// if(season.getMonth()==1||season.getMonth()==11||season.getMonth()==0){
+//     return 'winter'
+// }
+// if(season.getMonth()==2||season.getMonth()==3||season.getMonth()==4){
+//     return 'spring'
+// }
+// if(season.getMonth()==5||season.getMonth()==6||season.getMonth()==7){
+//     return 'summer'
+// }
+// if(season.getMonth()==8||season.getMonth()==9||season.getMonth()==10){
+//     return 'autumn'
+// }
+// else if(season===''){
+//     return 'corretly handles argument absence'
+//   }
+  
+
+
+// }
+// else {
+//     return 'Invalid date!'
+// }
 }
 
 module.exports = {
