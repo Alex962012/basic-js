564a6c0e2bfba73572c75a4c7a35fe8b7c363fe8
alex96 <alex.96_2012@mail.ru>
2022-05-01 22:48:34 +0900

    add what season\

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    
+
+let score=0
+if(Array.isArray(arr)){
+  score=+1
+}
+function flat (arr){
+arr.forEach((item)=>{
+
+ if(Array.isArray(item)){
+   flat(item)
+   score=score+1
+  
+ }
+})
+
+}
+
+flat(arr)
+return score
   }
 }
 
